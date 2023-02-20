b13819471ed63b86f78d61073cc8765c503ed5f2
Michael David Wilson <michaelwilson1991@gmail.com>
2023-02-20 15:19:42 +0800

    Sad face

--- notes/HRI_functions.R
+++ notes/HRI_functions.R
@@ -4,6 +4,7 @@ library(tidyverse)
 start_date = as.POSIXct("2018-05-01 08:00:00")
 
 bitvector_sequence = rep(rep(c(0,1,0), 6), each = 8)
+
 FIPSdf_from_bitvec = parse_sleepwake_sequence(
   seq = bitvector_sequence,
   series.start = start_date,
@@ -18,8 +19,7 @@ FIPS_work_df = FIPSdf_from_bitvec %>%
     switch_direction == "Wake" ~ "Rest",
     switch_direction == "Sleep" ~ "Work"
   )) %>%
-  mutate(work_status_int  = 1 - work_status_int,
-         work_status = !work_status)
+  mutate(work_status_int  = 1 - work_status_int, work_status = !work_status)
 
 
 # Risk_C = build up of risk
@@ -50,7 +50,7 @@ CT0 = 1
 # Functions Core
 # !! ----------- !!
 
-# Cummulative function
+# Cumulative function
 risk_C_function = function(prev_risk_c, gap_since_last_work, commute_time) {
   risk_c = 1 + (prev_risk_c - 1) * (exp(-0.7888 * (gap_since_last_work - 2 * commute_time) / 24))
   return(risk_c)
@@ -63,7 +63,14 @@ tod_component = function(decimal.time) {
   return(risk)
 }
 
+get_shift_type = function(decimalhour_vector) {
+# Case when style return from decinal hours
+# TODO: Note set the shift-type at the start of loop in one go
+}
 
+risk_C_dutyLevel = function() {
+
+}
 
 
 
@@ -97,6 +104,7 @@ for (i in 1:nrow(work_df)) {
   if (i > 1 & !work_df$work_status[i]) {
     if (work_df$risk_c[i - 1] == 1) {
       work_df$risk_c[i] = 1
+      # This should actually be T0, not 1
 
     # If it's not the first in the sequence, or if the prior value is not 1, then we need to recalculate (i.e., recovery)
     } else {
@@ -107,18 +115,34 @@ for (i in 1:nrow(work_df)) {
     }
   }
 
+  # TODO: Note that we need to set the cummulative risk at the shift level
+
   # Now we work out on shift risk (cumulative component risk C)
   if (work_df$work_status[i] & work_df$change_point[i] == 1) {
     if (work_df$shift.id[i] > 1) {
 
+
+      # Main cummulative risk on SHIFT level
+          }
+    if (RSE_data$duty_or_off[i] == "duty" & RSE_data$shift_seq[i] > 1 ) { 
+      
+    #   if (RSE_data$gap_previous[i] >= 9) {
+    #     RSE_data$Risk_C_formula[i] = "Sequence"
+    #     if (RSE_data$shift_type[i]  == "E") {C_up_exp = v_C_up_exp_E }
+    #     if (RSE_data$shift_type[i]  == "L") {C_up_exp = v_C_up_exp_L }
+    #     if (RSE_data$shift_type[i]  == "N") {C_up_exp = v_C_up_exp_N }
+    #     # old: RSE_data$Risk_C[i] =   Risk_C_first_in_seq*exp(C_up_exp*(RSE_data$shift_seq[i]-1))    
+    #     RSE_data$Risk_C[i] =   RSE_data$Risk_C[i-1]*exp(C_up_exp)    
+
+
+
       # Quick returns
       if(work_df$total_prev[i] <= v_QR_threshold) {
         work_df$risk_c[i] = work_df$risk_c[i-1] + v_QR_c*(v_QR_threshold - work_df$total_prev[i])}
-
     }
   }
 
-# End Loop
+
 }
 
 
@@ -126,3 +150,5 @@ for (i in 1:nrow(work_df)) {
 within(work_df, {
   HRI <- risk_c * risk_tod * risk_hos
 })
+
+view(work_df)
